8bc9388480af1f4ea8a8c2ddc8538e47af89ec80
ooskdud <ooskdud@gmail.com>
2021-05-13 21:08:26 +0900

    1

---
+++ post/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+from .models import Blog
+
+class CreatePostForm(forms.ModelForm):
+    class Meta:
+        model = Blog
+        fields = ['title', 'body']
+
+        
+   
